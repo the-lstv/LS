5f0d29928022f85a6952444ddbe1818bbc747f56
Lukáš Zloch <lstv@mail.com>
2026-09-15 00:47:34 +0200

    Update effect.js

--- js/effect.js
+++ js/effect.js
@@ -1,13 +1,16 @@
 /**
- * LS.js component for adding effects, reactivity, and creating fluid user interfaces that feel alive.
+ * LS.js component for adding effects, reactivity, animation and fluid motion to elements so they feel alive.
  * 
- * Primarily it further abstracts TouchHandle, but uses a global resolution focusing
- * on removing per-element overhead and light per-element state management & animation.
+ * It's API primarily further abstracts TouchHandle, using a global handle focusing
+ * on removing per-element overhead and having lightweight per-element state to avoid heavy abstractions.
  * 
  * It also serves as a general utility for managing element state or special behavior.
  * 
  * In short, it allows adding interactive/stateful animations and hooks to large amounts of elements
  * without having to do heavy setup.
+ * 
+ * It is also very easy to attach to elements via the `data-ls-effect` attribute, and extend with custom effects with minimal boilerplate.
+ * Effects use static methods and avoid instances per element, and state remains temporary.
  * 
  * Compatible with LS 6.0.0
  * 
@@ -21,6 +24,34 @@
  * @experimental
  * 
  * Last modified: 2026
+ * 
+ * Example effect:
+ * @example
+ * class MyEffect {
+ *     static { LS.Effect.register("my-effect", this) }
+ * 
+ *     // An effect is an object with dragStart, move, release, wheel, scroll, enter, leave, hover and other static methods.
+ * 
+ *     static dragStart(event, data) {
+ *         // event is a LS.Util.TouchHandle event, data is a temporary object for this effect instance's data.
+ *         // Don't store anything non-trivial in data and keep in mind it can be discarded.
+ *         // data.options contains options passed to the effect attribute, e.g. "my-effect:option1;option2" -> data.options = "option1;option2"
+ *         // To get the current element, use "this".
+ *
+ *         // All callbacks can return an object that can contain transform modifiers, animations, or other state changes. Order matters and is ordered by the attribute string.
+ *         return { scaleBy: 0.1 };
+ *     }
+ * 
+ *     static move(event, data) {
+ *         // Called when the element is moved while being dragged
+ *         return { translateBy: [event.dx, event.dy] };
+ *     }
+ * 
+ *     static release(event, data) {
+ *         // Called when the element is released after being dragged
+ *         return { translate: [0, 0], scale: 1 };
+ *     }
+ * }
  */
 
 class EffectManager extends LS.Component {
@@ -41,8 +72,10 @@ class EffectManager extends LS.Component {
         super();
 
         this.sharedHandle = new LS.Util.TouchHandle(document, {
-            // handleHover: true,
-            frameTimed: true,
+            frameTimed: true,  // Move events are throttled to animation frames
+
+            // handleHover: true, // Handle hover events (todo: test performance)
+            // handleWheel: true, // Handle wheel events (todo: test performance)
             // inertia: true,
 
             onStart: (event) => {
@@ -50,7 +83,7 @@ class EffectManager extends LS.Component {
                 const target = domEvent.target.closest("[data-ls-effect]");
                 if (!target || !(target instanceof HTMLElement)) return event.cancel();
 
-                // Reset handle state that could have been modified
+                // Reset handle state that could have been modified by previous effects
                 this.sharedHandle.options.pointerLock = false;
                 this.sharedHandle.options.calculateBounds = false;
                 this.sharedHandle.cursor = "auto";
@@ -83,19 +116,19 @@ class EffectManager extends LS.Component {
                 }
 
                 // sadly we iterate 2x for consistency
-                this._iterateTarget(event, (fx, data, target) => fx.dragStart?.call?.(target, event, data));
+                this._iterateTarget(event, (fx, data, target) => typeof fx.dragStart === "function"? fx.dragStart.call(target, event, data): fx.dragStart);
             },
 
             onMove: (event) => {
-                this._iterateTarget(event, (fx, data, target) => fx.move?.call?.(target, event, data));
+                this._iterateTarget(event, (fx, data, target) => typeof fx.move === "function"? fx.move.call(target, event, data): fx.move);
             },
 
             onScroll: (event) => {
-                this._iterateTarget(event, (fx, data, target) => fx.wheel?.call?.(target, event, data));
+                this._iterateTarget(event, (fx, data, target) => typeof fx.wheel === "function"? fx.wheel.call(target, event, data): fx.wheel);
             },
 
             onHover: (event) => {
-                this._iterateTarget(event, (fx, data, target) => fx.hover?.call?.(target, event, data));
+                this._iterateTarget(event, (fx, data, target) => typeof fx.hover === "function"? fx.hover.call(target, event, data): fx.hover);
             },
 
             onEnd: (event) => {
@@ -105,7 +138,7 @@ class EffectManager extends LS.Component {
     }
 
     releaseAll(event, _destroying = false) {
-        this._iterateTarget(event, (fx, data, target) => fx.release?.call?.(target, event, data), _destroying);
+        this._iterateTarget(event, (fx, data, target) => typeof fx.release === "function"? fx.release.call(target, event, data): fx.release, _destroying);
         this.currentEffects.length = 0;
     }
 
@@ -124,51 +157,85 @@ class EffectManager extends LS.Component {
         }
     }
 
+    *modifierIterator(value, count, fallback = 0) {
+        if(typeof value === "object" && !Array.isArray(value)) {
+            yield value.x === undefined? fallback: value.x;
+            yield value.y === undefined? fallback: value.y;
+            yield value.z === undefined? fallback: value.z;
+            return;
+        }
+
+        if(Array.isArray(value)) {
+            for(let i = 0; i < count; i++) {
+                yield value[i] === undefined? fallback: value[i];
+            }
+        } else {
+            for(let i = 0; i < count; i++) {
+                yield value === undefined? fallback: value;
+            }
+        }
+    }
+
     processModifier(data, event, target) {
         if(typeof data !== "object") return;
 
         const transform = target.__lsTransform;
         const fx = target.__lsEffect;
 
-        // todo: first do data.<x>, then apply data.<x>By
-        // todo: better fields apply & normalize
+        // todo: maybe first do data.<x>, then apply data.<x>By
         // todo: better units/css fields
         // todo: maybe better source for transforms
 
+        if(data.text) {
+            target.textContent = data.text;
+        }
+
         if(data.translate) {
-            transform.translate[0] = data.translate[0] || 0;
-            transform.translate[1] = data.translate[1] || 0;
-            transform.translate[2] = data.translate[2] || 0;
+            let i = 0;
+            for(const value of this.modifierIterator(data.translate, 3, 0)) {
+                if(value !== null) transform.translate[i] = value;
+                i++;
+            }
         }
 
         if(data.translateBy) {
-            transform.translate[0] += data.translateBy[0] || 0;
-            transform.translate[1] += data.translateBy[1] || 0;
-            transform.translate[2] += data.translateBy[2] || 0;
+            let i = 0;
+            for(const value of this.modifierIterator(data.translateBy, 3, 0)) {
+                if(value !== null) transform.translate[i] += value;
+                i++;
+            }
         }
 
         if(data.rotate) {
-            transform.rotate[0] = data.rotate[0] || 0;
-            transform.rotate[1] = data.rotate[1] || 0;
-            transform.rotate[2] = data.rotate[2] || 0;
+            let i = 0;
+            for(const value of this.modifierIterator(data.rotate, 3, 0)) {
+                if(value !== null) transform.rotate[i] = value;
+                i++;
+            }
         }
 
         if(data.rotateBy) {
-            transform.rotateBy[0] += data.rotateBy[0] || 0;
-            transform.rotateBy[1] += data.rotateBy[1] || 0;
-            transform.rotateBy[2] += data.rotateBy[2] || 0;
+            let i = 0;
+            for(const value of this.modifierIterator(data.rotateBy, 3, 0)) {
+                if(value !== null) transform.rotate[i] += value;
+                i++;
+            }
         }
 
         if(data.scale) {
-            if(!Array.isArray(data.scale)) data.scale = [data.scale, data.scale]; // todo
-            transform.scale[0] = data.scale[0] || 0;
-            transform.scale[1] = data.scale[1] || 0;
+            let i = 0;
+            for(const value of this.modifierIterator(data.scale, 2, 1)) {
+                if(value !== null) transform.scale[i] = value;
+                i++;
+            }
         }
 
         if(data.scaleBy) {
-            if(!Array.isArray(data.scaleBy)) data.scaleBy = [data.scaleBy, data.scaleBy]; // todo
-            transform.scale[0] += data.scaleBy[0] || 0;
-            transform.scale[1] += data.scaleBy[1] || 0;
+            let i = 0;
+            for(const value of this.modifierIterator(data.scaleBy, 2, 0)) {
+                if(value !== null) transform.scale[i] += value;
+                i++;
+            }
         }
 
         const animations = Array.isArray(data.animation)? data.animation: Array.isArray(data.animations)? data.animations: data.animation? [data.animation]: null;
@@ -220,7 +287,7 @@ class EffectManager extends LS.Component {
         return target.removeAttribute("data-ls-effect");
     }
 
-    register(effect, name) {
+    register(name, effect) {
         // if(!LS.Util.isClass(effect)) throw new Error("Invalid effect passed to LS.Effect.register");
         this.effects.set(name, effect);
     }
@@ -253,37 +320,48 @@ class Effect {
 }
 
 /**
- * When this effect is applied to an element, it will move towards the mouse and spring back when dragged.
+ * When this effect is applied to an element, it will move towards the mouse when dragged and spring back when released.
  */
 class Spring {
-    static { LS.Effect.register(this, "spring") }
+    static { LS.Effect.register("spring", this) }
 
     static dragStart(event, data) {
         event.preventDefault = false;
 
         data.moveX = true;
         data.moveY = true;
+        this.max      = 8;
+        this.strength = 0.18;
 
         if(data.options) {
             data.moveX = false;
             data.moveY = false;
-            if(data.options.indexOf("x") !== -1) data.moveX = true;
-            if(data.options.indexOf("y") !== -1) data.moveY = true;
+
+            data.options.split(";").forEach(option => {
+                const [key, value] = option.split("=");
+                if(key === "max") this.max = parseFloat(value) || this.max;
+                if(key === "strength") this.strength = parseFloat(value) || this.strength;
+                if(key === "x") data.moveX = true;
+                if(key === "y") data.moveY = true;
+                console.log("Spring effect option:", key, value, data.moveX, data.moveY, this.max, this.strength);
+            });
         }
+
+        data.box = this.getBoundingClientRect();
+        data.ofs = [event.x - data.box.left, event.y - data.box.top];
+
     }
 
     static move(event, data) {
-        const x = event.offsetX - (this.offsetWidth  / 2);
-        const y = event.offsetY - (this.offsetHeight / 2);
-    
-        const max      = 16;
-        const strength = 0.18;
+        const x = event.offsetX - (data.box.width  / 2) + data.ofs[0];
+        const y = event.offsetY - (data.box.height / 2) + data.ofs[1];
+
     
         const distance = Math.hypot(x, y);
         if (!distance || distance < 2) return { translate: [0, 0] };
 
         // Compress movement as it approaches the limit.
-        const amount = max * (1 - Math.exp(-distance * strength / max));
+        const amount = this.max * (1 - Math.exp(-distance * this.strength / this.max));
 
         return {
             translate: [
@@ -306,3 +384,39 @@ class Spring {
         } }
     }
 }
+
+/**
+ * Element will scale down when pressed and scale back up when released.
+ * I guess this could also be done via CSS.
+ * 
+ * Same as element:active { transform: scale(0.95); transition: transform 150ms ease-out; }
+ */
+LS.Effect.register("push", {
+    dragStart: {
+        animation: {
+            keyframes: [
+                { scale: "1 1" },
+                { scale: "0.95 0.95" }
+            ],
+            animationOptions: {
+                duration: 150,
+                easing: "ease-out",
+                fill: "forwards"
+            }
+        }
+    },
+
+    release: {
+        animation: {
+            keyframes: [
+                { scale: "0.95 0.95" },
+                { scale: "1 1" }
+            ],
+            animationOptions: {
+                duration: 150,
+                easing: "ease-out",
+                fill: "forwards"
+            }
+        }
+    }
+});
